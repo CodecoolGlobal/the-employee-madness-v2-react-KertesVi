04d161f2f120a45a92b0a4c4d98a1ced1d2d814f
KertesVi <kerteszviki@gmail.com>
2024-02-22 10:35:12 +0100

    prep

--- client/src/Pages/EmployeeCreator.jsx
+++ client/src/Pages/EmployeeCreator.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import EmployeeForm from "../Components/EmployeeForm";
-import Loading from "../Components/Loading";
+
 
 const createEmployee = (employee) => {
   return fetch("/api/employees", {
